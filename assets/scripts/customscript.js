5855a8dc4b698312af93eadc2bbdab0108783aa1
Henrikki <henrikki.gren@hotmail.com>
2024-10-08 17:34:52 +0300

    added a navigation to index.html and added a side nav also modified dropdown a li color to be black

--- assets/scripts/customscript.js
+++ assets/scripts/customscript.js
@@ -1,1 +1,16 @@
-AOS.init();
+AOS.init({
+    offset: 120,
+    duration: 1000
+});
+
+$(document).ready(function(){
+    $(' .sidenav').sidenav();
+    $('.dropdown-trigger').dropdown({
+        alignment: 'left',
+        inDuration: 300,
+        outDuration: 225,
+        coverTrigger: false,
+        constrainWidth: false,
+        closeOnClick: true
+      });
+});
