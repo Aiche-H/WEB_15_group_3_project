2d7d65b2d0adad344881bcc3d84b88f5d857ffbb
Markus <mp005840@edu.taitotalo.fi>
2025-04-07 21:30:43 +0300

    GET.js

--- database/methods/GET.js
+++ database/methods/GET.js
@@ -1,1 +1,12 @@
-GET;
+const Product = require("../../../models/Product");
+
+
+async function GET(context) {
+  context.log("GET.js aloitetaan...");
+
+  const products = await Product.find();
+  return { status: 200, jsonBody: products };
+}
+
+
+module.exports = { GET };
