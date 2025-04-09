ec8f41919be12e340d520c29ffd8ac4bf9ca48d2
Aiche-H <henrikki.gren@hotmail.com>
2025-04-09 13:55:09 +0300

    POST.js for users written

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 // models/user.js (updated)
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true },
@@ -7,17 +7,14 @@ const userSchema = new mongoose.Schema({
   password: String,
   first_name: String,
   last_name: String,
-  role: { type: String, default: 'user' },
+  role: { type: String, default: "user" },
   avatar: String,
   registration_date: { type: Date, default: Date.now },
   last_login: Date,
   last_activity: Date,
   email_verification: { type: Boolean, default: false },
-  verification_token: String
+  verification_token: String,
 });
 
-module.exports = mongoose.model('User', userSchema, 'users'); // Force collection 'users'
-
-
-
+module.exports = mongoose.model("User", userSchema, "users"); // Force collection 'users'
 
